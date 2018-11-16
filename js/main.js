b3b3bf4e8cafb664e3ac1abe452de31bef404f49
Gregory <gregory-starodubcev@yandex.ru>
2018-11-17 01:42:32 +0700

    Second version that actually works. Added functionality and some minor style improvements

--- js/main.js
+++ js/main.js
@@ -1,31 +1,57 @@
 const d = document;
+const options = ['rock', 'paper', 'scissors'];
 const templates = [...d.querySelectorAll('template')];
 const questionTemplate = d.querySelector('#question');
-const opponentLinks = d.querySelector('.player__options--player');
+const playerOptions = d.querySelector('.player__options--player');
+const playerLinks = [...d.querySelectorAll('.player__choice')];
 const arenaPlayer = d.querySelector('.arena__player');
+const arenaOpponent = d.querySelector('.arena__opponent');
 const sides = [...d.querySelectorAll('.arena__default')];
-opponentLinks.addEventListener('click', opponentLinksEventHandler, false);
+const play = d.querySelector('.arena__button');
+const stars = [...d.querySelectorAll('.fa-star')];
+let humanStars = [...d.querySelectorAll('.points__player--human .fa-star')];
+let computerStars = [...d.querySelectorAll('.points__player--computer .fa-star')];
+const players = [
+  {type: 'human', choice: null, wins: 0, stars: humanStars, img: arenaPlayer}, 
+  {type: 'computer', choice: null, wins: 0, stars: computerStars, img: arenaOpponent}
+];
+let map = {};
 
-sides.forEach(side => {
-  let clonedTemplate = questionTemplate.content.cloneNode(true);
-  side.appendChild(clonedTemplate);
+options.forEach((opt, i) => {
+  map[opt] = {};
+  map[opt][opt] = 0;
+  map[opt][options[(i + 1) % options.length]] = -1;
+  map[opt][options[(i + 2) % options.length]] = 1;
 });
 
-function opponentLinksEventHandler(e) {
+playerOptions.addEventListener('click', playerOptionsClickHandler, false);
+play.addEventListener('click', playClickHandler, false);
+
+// START ROUND
+resetChoices();
+
+function playClickHandler() {
+  if(players[0].choice != null) {
+    setComputerChoice();
+    makeTurn();
+  }
+}
+
+function playerOptionsClickHandler(e) {
   if(e.target !== e.currentTarget) {
-    const type = getChoice(e);
-    setChosenPicture(type);
+    setChoice(e.target);
   }
   e.stopPropagation();
   e.preventDefault();
 }
 
-function getChoice(e) {
-  let choice = e.target.getAttribute('data-player-choice');
+function getChoice(data) {
+  let choice = data.getAttribute('data-player-choice');
   return choice;
 }
 
-function setChosenPicture(type) {
+function setChoice(ch) {
+  const type = getChoice(ch);
   for(let i = 0; i < templates.length; i++) {
     if(templates[i].id === type) {
       let clonedTemplate = templates[i].content.cloneNode(true);
@@ -38,4 +64,102 @@ function setChosenPicture(type) {
       break;
     }
   }
+  players[0].choice = type;
+}
+
+function randomFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function setComputerChoice() {
+  players[1].choice = randomFromArray(options);
+  
+  for(let i = 0; i < templates.length; i++) {
+    if(templates[i].id === players[1].choice) {
+      let clonedTemplate = templates[i].content.cloneNode(true);
+      if(arenaOpponent.hasChildNodes()) {
+        // arenaOpponent.removeChild(arenaOpponent.firstChild);
+        arenaOpponent.innerHTML = '';
+      } 
+      arenaOpponent.appendChild(clonedTemplate);
+
+      break;
+    }
+  }  
+}
+
+function compareChoices(ch1, ch2) {
+  if(ch1 != null && ch2 != null) {
+    return map[ch1][ch2];
+  }
+  throw new Error('Invalid choice');
+}
+
+function makeTurn() {
+  const result = compareChoices(players[0].choice, players[1].choice);
+  if(result == null) {
+    throw new Error('Invalid arguments');
+  } 
+
+  if(result === 0) {
+    setTimeout(resetChoices, 500);
+  } else if(result === 1) {
+    addWinPoint(players[0]);
+    animateChoice(players[0].img, players[1].img);
+    setTimeout(resetChoices, 2500);
+  }  else if(result === -1) {
+    addWinPoint(players[1]);
+    animateChoice(players[1].img, players[0].img);
+    setTimeout(resetChoices, 2500);
+  }
+}
+
+function animateChoice(winner, looser) {
+  winner.classList.add('bounceIn');
+  looser.classList.add('hinge');
+  playerLinks.forEach(link => link.style.pointerEvents = 'none');
+  setTimeout(function() {
+    winner.classList.remove('bounceIn');
+    looser.classList.remove('hinge');
+  }, 2000);
+}
+
+function addWinPoint(player) {
+  if(player.wins < 3) {
+    player.wins++;
+    addStar(player.stars, player.wins);
+  }
+  if(player.wins === 3) announceWinner(player);
+}
+
+function addStar(items, count) {
+  for(let i = 0; i < count; i++) {
+    items[i].classList.add('checked');
+  }
+}
+
+function announceWinner(pl) {
+  setTimeout(function() {
+    alert(`${pl.type} won!`);
+    resetGame();
+  }, 1000);
+}
+
+function resetChoices() {
+  sides.forEach(s => s.innerHTML = '');
+  sides.forEach(side => {
+    let clonedTemplate = questionTemplate.content.cloneNode(true);
+    side.appendChild(clonedTemplate);
+  });
+  playerLinks.forEach(link => link.style.pointerEvents = 'auto');
+}
+
+function resetWins() {
+  resetChoices();
+  players.forEach(pl => pl.wins = 0);
+  stars.forEach(st => st.classList.remove('checked'));
+}
+
+function resetGame() {
+  resetWins();
 }
